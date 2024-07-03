5f7a74f8ab677674e3f7af9e720e80dc737e532c
Jia Jia <mathjiajia@outlook.com>
2024-07-03 21:00:16 +0800

    feat: use builtin

--- lua/latex/module/texlab.lua
+++ lua/latex/module/texlab.lua
@@ -56,26 +56,6 @@ local toggle_star = function()
 	end
 end
 
-local change_env = function()
-	local bufnr = api.nvim_get_current_buf()
-	local params = lsp.util.make_position_params()
-
-	if not lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1] then
-		return vim.notify("Texlab client not found", vim.log.levels.ERROR)
-	end
-
-	local new_name = vim.fn.input("Enter the new environment name: ")
-	if not new_name or new_name == "" then
-		return vim.notify("No environment name provided", vim.log.levels.WARN)
-	end
-
-	params.newName = new_name
-	vim.lsp.buf.execute_command({
-		command = "texlab.changeEnvironment",
-		arguments = { params },
-	})
-end
-
 function M.init(config)
 	if not config.enabled then
 		return
@@ -85,7 +65,7 @@ function M.init(config)
 	vim.keymap.set("n", config.build, vim.cmd.TexlabBuild, { buffer = true, desc = "Build the current buffer" })
 	vim.keymap.set("n", config.forward, vim.cmd.TexlabForward, { buffer = true, desc = "Forward search from current position" })
 	vim.keymap.set("n", config.cancel_build, vim.cmd.TexlabCancelBuild, { buffer = true, desc = "Cancel the current build" })
-	vim.keymap.set("n", config.change_env, change_env, { buffer = true, desc = "Change the current environment" })
+	vim.keymap.set("n", config.change_env, vim.cmd.TexlabChangeEnvironment, { buffer = true, desc = "Change the current environment" })
 	vim.keymap.set("i", config.close_env, close_env, { buffer = true, desc = "Close the current environment" })
 	vim.keymap.set("n", config.toggle_star, toggle_star, { buffer = true, desc = "Toggle starred environment" })
 	-- stylua: ignore end
