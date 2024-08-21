495a3d2c8eda33f986aa675c5773b2b19ba5e03f
Jia Jia <mathjiajia@outlook.com>
2024-08-21 22:41:11 +0800

    chore(texlab): update

--- lua/latex/module/texlab.lua
+++ lua/latex/module/texlab.lua
@@ -4,62 +4,59 @@ local M = {}
 
 local close_env = function()
 	local bufnr = api.nvim_get_current_buf()
-	local client = lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1]
-	local params = lsp.util.make_position_params()
-
-	if client then
-		client.request("workspace/executeCommand", {
-			command = "texlab.findEnvironments",
-			arguments = { params },
-		}, function(err, result)
-			if err then
-				return vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
-			end
-			if #result == 0 then
-				return vim.notify("No environment found", vim.log.levels.WARN)
-			end
-
-			local text = result[#result].name.text
-			api.nvim_put({ "\\end{" .. text .. "}" }, "", false, true)
-		end, bufnr)
-	else
-		vim.notify(
-			"method textDocument/executeCommand is not supported by any servers active on the current buffer",
-			vim.log.levels.WARN
-		)
+	local texlab_client = lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1]
+	if not texlab_client then
+		return vim.notify("Texlab client not found", vim.log.levels.ERROR)
 	end
+
+	texlab_client.request("workspace/executeCommand", {
+		command = "texlab.findEnvironments",
+		arguments = { lsp.util.make_position_params() },
+	}, function(err, result)
+		if err then
+			return vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
+		end
+		if #result == 0 then
+			return vim.notify("No environment found", vim.log.levels.WARN)
+		end
+
+		local text = result[#result].name.text
+		api.nvim_put({ "\\end{" .. text .. "}" }, "", false, true)
+	end, bufnr)
 end
 
 local toggle_star = function()
 	local bufnr = api.nvim_get_current_buf()
-	local client = lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1]
-	local params = lsp.util.make_position_params()
-
-	if client then
-		client.request("workspace/executeCommand", {
-			command = "texlab.findEnvironments",
-			arguments = { params },
-		}, function(err, result)
-			if err then
-				return vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
-			end
-			if #result == 0 then
-				return vim.notify("No environment found", vim.log.levels.WARN)
-			end
-
-			local text = result[#result].name.text
-			params.newName = text:sub(-1) == "*" and text:sub(1, -2) or text .. "*"
-			vim.lsp.buf.execute_command({
-				command = "texlab.changeEnvironment",
-				arguments = { params },
-			})
-		end, bufnr)
-	else
-		vim.notify(
-			"method textDocument/executeCommand is not supported by any servers active on the current buffer",
-			vim.log.levels.WARN
-		)
+	local texlab_client = lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1]
+	if not texlab_client then
+		return vim.notify("Texlab client not found", vim.log.levels.ERROR)
 	end
+
+	texlab_client.request("workspace/executeCommand", {
+		command = "texlab.findEnvironments",
+		arguments = { lsp.util.make_position_params() },
+	}, function(err, result)
+		if err then
+			return vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
+		end
+		if #result == 0 then
+			return vim.notify("No environment found", vim.log.levels.WARN)
+		end
+
+		local text = result[#result].name.text
+		local new = text:sub(-1) == "*" and text:sub(1, -2) or text .. "*"
+		local pos = vim.api.nvim_win_get_cursor(0)
+		vim.lsp.buf.execute_command({
+			command = "texlab.changeEnvironment",
+			arguments = {
+				{
+					textDocument = { uri = vim.uri_from_bufnr(bufnr) },
+					position = { line = pos[1] - 1, character = pos[2] },
+					newName = new,
+				},
+			},
+		})
+	end, bufnr)
 end
 
 function M.init(config)
